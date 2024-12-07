52a26cca789bb7ac95d28efeb22acd1483913602
Adejumo Adedamola <damoladavidadejumo@gmail.com>
2024-12-07 21:42:00 +0100

    updated navbar

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Navbar from "./navbar";
+const App = () =>{
+    return(
+        <Navbar/>
+    )
+}
+export default App;
---
+++ src/navbar.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <> <ul
+      data-orientation="horizontal"
+      className="group flex flex-1 list-none items-center justify-center space-x-1 w-[1100px] top-20 bg-transparent"
+      dir="ltr"
+    >
+      <li className="top-20 border-none">
+        <button
+          id="radix-:rh:-trigger-radix-:ri:"
+          data-state="closed"
+          aria-expanded="false"
+          aria-controls="radix-:rh:-content-radix-:ri:"
+          className="group text-[#FAFAFA] hover:text-[#69FF93] data-[active]:text-[#69FF93] data-[state=open]:text-[#69FF93] data-[active]:bg-[#69FF9329] data-[state=open]:bg-[#69FF9329] inline-flex h-10 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 hover:bg-[#69FF9329] group"
+          data-radix-collection-item=""
+        >
+          Ecosystem
+        </button>
+      </li>
+      <li className="top-20 border-none">
+        <button
+          id="radix-:rh:-trigger-radix-:rj:"
+          data-state="closed"
+          aria-expanded="false"
+          aria-controls="radix-:rh:-content-radix-:rj:"
+          className="group text-[#FAFAFA] hover:text-[#69FF93] data-[active]:text-[#69FF93] data-[state=open]:text-[#69FF93] data-[active]:bg-[#69FF9329] data-[state=open]:bg-[#69FF9329] inline-flex h-10 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 hover:bg-[#69FF9329] group"
+          data-radix-collection-item=""
+        >
+          Products
+        </button>
+      </li>
+      <li className="top-20 border-none">
+        <button
+          id="radix-:rh:-trigger-radix-:rk:"
+          data-state="closed"
+          aria-expanded="false"
+          aria-controls="radix-:rh:-content-radix-:rk:"
+          className="group text-[#FAFAFA] hover:text-[#69FF93] data-[active]:text-[#69FF93] data-[state=open]:text-[#69FF93] data-[active]:bg-[#69FF9329] data-[state=open]:bg-[#69FF9329] inline-flex h-10 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 hover:bg-[#69FF9329] group"
+          data-radix-collection-item=""
+        >
+          zNode
+        </button>
+      </li>
+      <li className="top-20 border-none">
+        <button
+          id="radix-:rh:-trigger-radix-:rl:"
+          data-state="closed"
+          aria-expanded="false"
+          aria-controls="radix-:rh:-content-radix-:rl:"
+          className="group text-[#FAFAFA] hover:text-[#69FF93] data-[active]:text-[#69FF93] data-[state=open]:text-[#69FF93] data-[active]:bg-[#69FF9329] data-[state=open]:bg-[#69FF9329] inline-flex h-10 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 hover:bg-[#69FF9329] group"
+          data-radix-collection-item=""
+        >
+          Build
+        </button>
+      </li>
+      <li className="top-20 border-none">
+        <button
+          id="radix-:rh:-trigger-radix-:rm:"
+          data-state="closed"
+          aria-expanded="false"
+          aria-controls="radix-:rh:-content-radix-:rm:"
+          className="group text-[#FAFAFA] hover:text-[#69FF93] data-[active]:text-[#69FF93] data-[state=open]:text-[#69FF93] data-[active]:bg-[#69FF9329] data-[state=open]:bg-[#69FF9329] inline-flex h-10 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50 hover:bg-[#69FF9329] group"
+          data-radix-collection-item=""
+        >
+          About Us
+        </button>
+      </li>
+    </ul>
+   </>
+  );
+};
+
+export default Navbar;
